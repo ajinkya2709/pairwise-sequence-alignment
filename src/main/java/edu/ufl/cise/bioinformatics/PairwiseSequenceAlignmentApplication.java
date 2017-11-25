631f51846beef06880be689c66c614182256b13f
Ajinkya Gaikwad <ajinkyagaikwad2709@gmail.com>
2017-11-25 15:19:39 -0500

    Added Spring Boot Skeleton

---
+++ src/main/java/edu/ufl/cise/bioinformatics/PairwiseSequenceAlignmentApplication.java
@@ -0,0 +1,12 @@
+package edu.ufl.cise.bioinformatics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairwiseSequenceAlignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PairwiseSequenceAlignmentApplication.class, args);
+	}
+}
